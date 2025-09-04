d62a7fcdc932a4a465da0530ef90436ccf4665a9
david-oliveira-silva <luceni0122@gmail.com>
2025-09-04 17:46:32 -0300

    Implementado CidadeService com validação de regras de negócio

---
+++ EM.Service/Service/CidadeService.cs
@@ -0,0 +1,60 @@
+﻿
+using EM.Domain.Enum;
+using EM.Domain.Models;
+using EM.Repository.Repository.Cidade;
+
+namespace EM.Service.Service
+{
+    public class CidadeService
+    {
+        private ICidadeRepository cidadeRepository;
+
+        public CidadeService(ICidadeRepository cidadeRepository)
+        {
+            this.cidadeRepository = cidadeRepository;
+        }
+
+        public void CadastrarCidade(string cidadeNome, UF cidadeUF)
+        {
+            if (string.IsNullOrEmpty(cidadeNome))
+            {
+                throw new Exception("Nome não pode ser vazio");
+            }
+
+           
+
+
+            var novacidade = new CidadeModel(cidadeNome, cidadeUF);
+
+            cidadeRepository.Cadastrar(novacidade);
+        }
+
+        public void DeletarCidade(CidadeModel cidadeModel)
+        {
+            if(cidadeModel == null)
+            {
+                throw new Exception("Cidade não encontrada");
+            }
+
+            cidadeRepository.Deletar(cidadeModel);
+        }
+
+        public void EditarCidade(CidadeModel cidadeModel)
+        {
+            if(cidadeModel == null)
+            {
+                throw new Exception("Cidade não encontrada");
+            }
+             if (string.IsNullOrEmpty(cidadeModel.cidadeNome))
+            {
+                throw new Exception("Nome não pode ser vazio");
+            }
+        }
+
+        public List<CidadeModel> ListarCidades()
+        {
+            var cidade = cidadeRepository.Listar();
+            return cidade.OrderBy(c=>c.cidadeID).ToList();
+        }
+    }
+}
